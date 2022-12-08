3d806ceaefd473dc9fd6949588d755122e9878c8
T19CS006-Ishikawa <takumi13226@192.168.0.11>
2022-12-09 01:34:53 +0900

    formタグ内を変更

--- mainApp/index.php
+++ mainApp/index.php
@@ -4,9 +4,8 @@
 		<meta http-equiv="Content-Type" content = "text/html;charset=UTF-8">
 	</head>
 	<body>
-		<form	enctype = "multipart/form-data" action = "./index.php"	method="POST">
-			<input type = "file" name = "upfile"><br>
-			<input type = "submit" value ="送信">
+		<form	enctype = "multipart/form-data" action = "./getFileName.php"	method="POST">
+			<input type = "submit" value ="取得">
 		</form>
 
 	</body>
