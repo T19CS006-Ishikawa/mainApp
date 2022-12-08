96d6f961ba2eb425b4612fb4ec889ad30b497774
T19CS006-Ishikawa <takumi13226@192.168.0.11>
2022-12-09 01:33:46 +0900

    test

--- mainApp/index.php
+++ mainApp/index.php
@@ -1,3 +1,14 @@
-<?php
+<!doctype html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content = "text/html;charset=UTF-8">
+	</head>
+	<body>
+		<form	enctype = "multipart/form-data" action = "./index.php"	method="POST">
+			<input type = "file" name = "upfile"><br>
+			<input type = "submit" value ="送信">
+		</form>
 
-?>
+	</body>
+</html>
+
