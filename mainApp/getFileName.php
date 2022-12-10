079b267db5f59f42f8a056405f997b58457a02de
T19CS006-Ishikawa <takumi13226@192.168.0.11>
2022-12-11 03:02:41 +0900

    while分変更

--- mainApp/getFileName.php
+++ mainApp/getFileName.php
@@ -3,14 +3,13 @@
 
 $fh = fopen('https://file-upload-app.herokuapp.com/getFileName.php', 'r');
 
-while($sRec = fgets($fh))
+$count = 0;
 
-{
-
- echo $sRec."<BR>";
-
+while($count <= count($fh) ){
+    echo $fh[$count];
+    echo '<br>';
+    $count++;
 }
-
 fclose($fh);
 
 ?>
