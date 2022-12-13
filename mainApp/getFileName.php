ec4e36110b60d8e8bf5dfadf954a5ce1f9bce000
T19CS006-Ishikawa <takumi13226@192.168.0.11>
2022-12-14 08:37:46 +0900

    clean

--- mainApp/getFileName.php
+++ mainApp/getFileName.php
@@ -1,14 +1,6 @@
 
 <?php
 $path = 'https://file-upload-app.herokuapp.com/getFileName.php';
-$fh = fopen($path, 'r');
-
-while($sRec = fgets($fh)){
-  echo $sRec."<BR>";
-
-}
-fclose($fh);
-
 $c = file_get_contents('https://file-upload-app.herokuapp.com/upfile/list.txt');
 echo $c;
 
