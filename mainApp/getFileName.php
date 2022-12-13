7425fe71e071ae629609b0c9a68f726cd57f07f8
T19CS006-Ishikawa <takumi13226@192.168.0.11>
2022-12-14 07:02:00 +0900

    url

--- mainApp/getFileName.php
+++ mainApp/getFileName.php
@@ -1,14 +1,17 @@
 
 <?php
-$path = 'https://file-upload-app.herokuapp.com/getFileName.php';
+$path = 'https://file-upload-app.herokuapp.com/';
+/*
 $fh = fopen($path, 'r');
 
 while($sRec = fgets($fh)){
  //echo $sRec."<BR>";
 }
+fclose($fh);
+*/
 $c = file_get_contents($path);
 echo $c;
 
-fclose($fh);
+
 
 ?>
