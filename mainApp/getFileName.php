94bf0c9378c68afc867e4672008f3ef10f849f3f
T19CS006-Ishikawa <takumi13226@192.168.0.11>
2022-12-14 04:21:04 +0900

    pathをlist.csvに変更

--- mainApp/getFileName.php
+++ mainApp/getFileName.php
@@ -1,13 +1,13 @@
 
 <?php
-$path = 'https://file-upload-app.herokuapp.com/getFileName.php';
+$path = 'https://file-upload-app.herokuapp.com/list.csv';
 $fh = fopen($path, 'r');
 $df = fgets($fh);
 
 while($sRec = fgets($fh)){
     
- echo $sRec;
- break;
+ echo $sRec."<BR>";
+
 }
 
 fclose($fh);
