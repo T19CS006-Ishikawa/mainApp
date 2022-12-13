292cecd30ff85f7dd8090c98ccb9ee64669d0ed0
T19CS006-Ishikawa <takumi13226@192.168.0.11>
2022-12-14 07:57:40 +0900

    nest

--- mainApp/getFileName.php
+++ mainApp/getFileName.php
@@ -1,16 +1,18 @@
 
 <?php
 $path = 'https://file-upload-app.herokuapp.com/upfile/';
-/*
+
 $fh = fopen($path, 'r');
 
 while($sRec = fgets($fh)){
  //echo $sRec."<BR>";
+  $c = file_get_contents($path.$sRec);
+  echo $c;
 }
 fclose($fh);
-*/
-$c = file_get_contents($path);
-echo $c;
+
+//$c = file_get_contents($path);
+//echo $c;
 
 
 
