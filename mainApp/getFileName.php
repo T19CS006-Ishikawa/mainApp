bd4b97016cdfb547e94cb6913a25940b2f68becb
T19CS006-Ishikawa <takumi13226@192.168.0.11>
2022-12-14 03:39:09 +0900

    whileを除外

--- mainApp/getFileName.php
+++ mainApp/getFileName.php
@@ -2,24 +2,15 @@
 <?php
 $path = 'https://file-upload-app.herokuapp.com/getFileName.php';
 $fh = fopen($path, 'r');
+$df = fgets($fh);
+echo $df;
 /*
-$data;
-$count= 0;
-while($cFile = fgets($fh)){
-    $data[count] = $cFile;
-    $count++;
-}
-$eCount = 0;
-while($eCount!=$count){
-    echo $data[$eCount];
-}*/
-
 while($sRec = fgets($fh))
 
 {
  echo $sRec."<BR>";
 }
-
+*/
 fclose($fh);
 
 ?>
