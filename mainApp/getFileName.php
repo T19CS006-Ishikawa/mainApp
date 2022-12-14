ef1e391608742a020fae7617da7bced244d58f57
T19CS006-Ishikawa <takumi13226@192.168.0.11>
2022-12-14 10:50:40 +0900

    名前を配列に格納

--- mainApp/getFileName.php
+++ mainApp/getFileName.php
@@ -1,8 +1,12 @@
 
 <?php
-$path = 'https://file-upload-app.herokuapp.com/getFileName.php';
 $c = file_get_contents('https://file-upload-app.herokuapp.com/upfile/list.txt');
-echo $c;
+//echo $c;
+
+//テキストファイルの名前を一行ごと配列に格納
+
+$file_array = file($c);/*一行ずつ格納*/
+echo $file_array;
 
 
 ?>
