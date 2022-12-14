d22a9169ebe71cf48b91d5fc03a84eed1cbf7fd0
T19CS006-Ishikawa <takumi13226@192.168.0.11>
2022-12-14 10:55:59 +0900

    printに

--- mainApp/getFileName.php
+++ mainApp/getFileName.php
@@ -1,12 +1,12 @@
 
 <?php
 $c = file_get_contents('https://file-upload-app.herokuapp.com/upfile/list.txt');
-echo $c;
+//echo $c;
 
 //テキストファイルの名前を一行ごと配列に格納
 
 $file_array = file($c);/*一行ずつ格納*/
-//echo $file_array;
+ print_r($file_array);
 
 
 ?>
