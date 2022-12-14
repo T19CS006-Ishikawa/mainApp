a0ab0dc7ba7dba572a82f310c80fa304669553ef
T19CS006-Ishikawa <takumi13226@192.168.0.11>
2022-12-14 14:53:02 +0900

    csv

--- mainApp/getFileName.php
+++ mainApp/getFileName.php
@@ -6,12 +6,31 @@ $c = file_get_contents('https://file-upload-app.herokuapp.com/upfile/list.txt');
 //mainApp内にtxtを実質コピーする
 $handle =  fopen("./mainList.txt","w");
     fputs($handle,$c);
-    readfile("mainList.txt");
+    //readfile("mainList.txt");
 fclose($handle);
 
+
+$path = "https://file-upload-app.herokuapp.com/upfile/";
+
+//行ごとに名前を格納
 $file_array = file("mainList.txt");
+/*
 foreach ($file_array as $name) {
     print $name."<br>";
 }
+*/
+$csv_array =[];
+$count = 0;
+ 
+ //csv抽出
+$fp = fopen($path.$file_array[$count]);
+   while($line = fgetcsv($fp)){
+        var_dump($line);
+        echo "<br />";
+    }
+ fclose($fp);
+
+    
+
 
 ?>
