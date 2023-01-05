5dde52f5fac21174983f520258f992b577db156c
T19CS006-Ishikawa <takumi13226@192.168.0.10>
2023-01-06 03:29:14 +0900

    ほんとに

--- mainApp/getCSV.php
+++ mainApp/getCSV.php
@@ -1,4 +1,5 @@
 <?php 
+$dlpath = 'https://file-Upload-app.herokuapp.com/upfile';
 $path = 'https://app-for-lms.herokuapp.com/csvData';
 
 //echo $c;
@@ -9,19 +10,23 @@ $read = file_get_contents($fp);
 $list = explode("|",$read );
 //var_dump($list);
 
-//list[0] = ファイル名になる
+//list[0] = csvファイル名になる
 $csvname = '/'.$list[0];
+
+$dlroot = $dlpath.$csvname;
 $root = $path.$csvname;
 
-echo $root;
+echo "ダウンロード元：".$dlroot;
+echo "<br>";
+echo "ダウンロード先：".$root;
 echo "<br>";
 
 
 
 // ダウンロード元のファイルパス（絶対パス、ファイル名まで含む）を指定する
-//$root
+$url = $dlroot;
 
-$data = file_get_contents($root);
+$data = file_get_contents($url);
 
 file_put_contents('./csvData',$data); //ファイルの保存先
 
