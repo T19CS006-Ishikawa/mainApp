4fdbc5892e168e0343e3a6a674acf076a1ee0c7d
T19CS006-Ishikawa <takumi13226@192.168.0.6>
2023-01-18 18:50:27 +0900

    var_dumo

--- mainApp/getFileName.php
+++ mainApp/getFileName.php
@@ -18,10 +18,11 @@ var_dump($list);
 for($num = 0; $num < count($list)-1;$num++){
     //ファイル名にステータスを追加
     $list_status[$num]= $list[$num]."not,not";
+    var_dump($list_status);
 //＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿ここまでは実行されている
     //ステータスを追加したものを新たに保存、ここでファイル名ごとにテキストファイルを作成する
-    $status_path = $path.$list[num].$status;
-    echo "done";
+    $status_path = $path.$list[num].$status;//ここで止まっている
+    echo "<br>";
     echo $status_path;
     $handle = fopen($status_path,'w');
     fputs($handle, $list_status[num]);
