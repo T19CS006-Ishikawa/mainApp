7d7dab319c6c428539e7477bf5640efca15fecc2
T19CS006-Ishikawa <takumi13226@192.168.0.6>
2023-01-20 16:08:50 +0900

    ed

--- mainApp/checkStatus.php
+++ mainApp/checkStatus.php
@@ -1,7 +1,20 @@
 <?php
 //提出ステータスのチェックを行い、notであれば日付をチェックし、リマインドする日であればメッセージを送信する。
 //まずステータスを取得するために、今あるテキストファイルのうちから*_status.txtを取得する
-$read = glob('./csvdata/*_status.txt');
-var_dump($read);
+$read = glob('./csvdata/*.txt');
+//var_dump($read);
+$count = 0;
+
+for($num = 0; $num < count($read);$num++){
+    $result[$num] = read[$num];
+    $test = strpos($result[$num], "_status.txt");
+    if($test !== false){
+        $unEdited[$count] = $result[$num];
+        $edited[$count] = substr($unEdited[$count], 10);
+        $count++;
+    }
+}
+
+var_dump($edited);
 
 ?>
