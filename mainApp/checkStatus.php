1fcacbbb3958b97958e2049e96b9e90abf4250a3
T19CS006-Ishikawa <takumi13226@192.168.0.6>
2023-01-21 03:15:20 +0900

    h

--- mainApp/checkStatus.php
+++ mainApp/checkStatus.php
@@ -22,7 +22,6 @@ for($num = 0; $num < count($read);$num++){
         $length = strlen($edited[$count]); //文字列の長さ
         $edited[$count] = substr($edited[$count],0,$length-11);
         
-        //$edited[$count] = substr($edited[$count],-11);
         $count++;
     }
 }
