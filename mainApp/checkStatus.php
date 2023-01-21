be9b3822be89383ab65ded2ae6cd61f262f60f71
T19CS006-Ishikawa <takumi13226@192.168.0.6>
2023-01-21 14:41:36 +0900

    date

--- mainApp/checkStatus.php
+++ mainApp/checkStatus.php
@@ -5,8 +5,7 @@ $csv_url = 'https://file-upload-app.herokuapp.com/upfile/';
 $url = 'https://app-for-lms.herokuapp.com/';
 
 $read = glob('./csvData/*.txt');
-var_dump($read);
-echo "<br>";
+
 $count = 0;
 $target = "not";
 
@@ -34,18 +33,44 @@ for($num = 0; $num < count($unEdited);$num++){
     $status = explode(',', $status_array[$num]);
     
     if(strcmp($status[2],$target) == 0){
-        echo "yeah"."<br>";
+        
        //file_get_contents($schedule_path);
        $get_path  = $csv_url.$edited[$num].".csv";
-       echo $get_path."<br>";
+       
        $read_csv = file_get_contents($get_path);
        $csv_array = explode(',',$read_csv);
-       var_dump($csv_array);
-       echo "<br>";
+       
        $date = $csv_array[1];  //日付を取得
        $date_array = explode('/',$date);
-       var_dump($date_array);
-       echo "<br>";
+       // 日付関数を用いる準備として年/月/日の部分をint型に変換
+       $int_year = (int)$date_array[0];
+       $int_month = (int)$date_array[1];
+       $int_date = (int)$date_array[2];
+       
+       //日付のチェック、ここでは提出期限もしくは試験日の１日前かをチェックする
+       if($int_date == 1){
+           if($int_month == 1){
+               $int_year--;
+               $int_month = 12;
+               $int_date = 31;
+           }else{
+               $int_month--;
+               if($int_month == 2){
+                   $int_date = 28;
+               }else if($int_month == 4 || $int_month == 6 || $int_month == 9 || $int_month == 11){
+                   $int_date = 30;
+               }else {
+                   $int_date = 31;
+               }
+                  
+           }
+           
+       }
+       
+       $target_date = $int_year.'/'.$int_month.'/'.$int_date;
+       $str_target_date = (string)$target_date;
+       echo $str_target_date."<br>";
+       
         
     }
 }
