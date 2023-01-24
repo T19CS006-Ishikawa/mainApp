7bf0b4c2f3f76263a9cd8848e1260096a82607fb
T19CS006-Ishikawa <takumi13226@192.168.11.9>
2023-01-25 03:33:30 +0900

    ok

--- mainApp/pushMessage.php
+++ mainApp/pushMessage.php
@@ -1,6 +1,6 @@
 <?php
 //LINE APIのプログラムを実行させてみる
-$LINE_path = 'https://line-bot-tester01.herokuapp.com/pushMessage.php';
+$LINE_path = 'https://line-bot-tester01.herokuapp.com/multiCast.php';
 file_get_contents($LINE_path);
 
  ?>
